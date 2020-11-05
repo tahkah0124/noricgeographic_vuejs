ff69d54e8558129648a85bc60b3767a2473a34e4
tahkah0124 <tahkah0124@gmail.com>
2020-11-05 21:42:35 +0900

    初期コミット

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <ExampleList msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import ExampleList from "./components/ExampleList.vue";
+
+export default {
+  name: "App",
+  components: {
+    ExampleList
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
